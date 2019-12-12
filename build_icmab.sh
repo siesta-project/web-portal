5d92808d5511c088766527fdf26fcaf7273c03ad
Alberto Garcia <albertog@icmab.es>
2019-12-12 10:32:48 +0100

    Prepare ICMAB deployment

--- build_icmab.sh
+++ build_icmab.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #
 # Commands to build the Siesta website from the Jekyll sources
+# PRODUCTION VERSION
+# USE local development settings to try things out before deployment!!
 #
 # For the ICMAB site
-# (Still marginally non-optimal, due to the long baseurl)
 #
 jekyll build --config _config_icmab.yml
+# The following is a specific directory that is symlinked to 'siesta'
 rsync -av _site/ internal.icmab.es:siesta-jekyll
 ssh internal.icmab.es "chgrp -R www-data siesta-jekyll"
 #
