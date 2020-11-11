382059b8fc6d38d52ebd18686879db345934babd
Alberto Garcia <albertog@icmab.es>
2020-11-11 23:25:32 +0100

    Update for hosting config. Update publications

--- _posts/2020-05-28-overview-paper.md
+++ _posts/2020-05-28-overview-paper.md
@@ -8,3 +8,6 @@ As part of a special issue on "Electronic Structure Software" in the Journal of 
 paper providing an overview of recent developments and sample applications of Siesta has been published:
 <a href="https://doi.org/10.1063/5.0005077">J. Chem. Phys. 152,
     204108 (2020)</a>.
+
+(A postprint open-access version is available <a href="https://digital.csic.es/bitstream/10261/213028/1/GarciaArribas_JChemPhys_2020_postprint.pdf">here</a>.)
+
---
+++ build_hosting.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Commands to build the Siesta website from the Jekyll sources
+# PRODUCTION VERSION
+# USE local development settings to try things out before deployment!!
+#
+# For the hosting site
+#
+jekyll build --config _config_icmab_hosting.yml
+#
+# You need some setup for rclone to work
+# Use 'copy' instead of 'sync' to avoid deleting files for now
+#
+rclone copy -P --no-traverse _site siesta_hosting:public_html/siesta
+#
+# For the GitHub pages site
+# (an implicit 'jekyll build' will be done automatically, using the
+#  default _config.yml file)
+#
+# For local development
+#
+#jekyll build --config _config_dev.yml
+
+
